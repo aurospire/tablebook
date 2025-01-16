4fe5c3f34cbd81332458187a818a32405028d3d3
aurospire <aurospire@gmail.com>
2025-01-16 04:23:37 -0500

    Refactor generate method in GoogleGenerator to include error handling with Result type

--- src/google/GoogleGenerator.ts
+++ src/google/GoogleGenerator.ts
@@ -13,98 +13,103 @@ export class GoogleGenerator implements SheetGenerator {
     }
 
     async generate(book: SheetBook): Promise<Result<undefined, TableBookGenerateIssue[]>> {
-        const sheet = this.#sheet;
+        try {
+            const sheet = this.#sheet;
 
-        // clear the sheet
-        const resetSheetId = await sheet.reset();
+            // clear the sheet
+            const resetSheetId = await sheet.reset();
 
-        await sheet.modify(r => r.setTitle(book.title));
-
-
-        for (const page of book.pages) {
-            const columnCount = page.groups.reduce((acc, group) => acc + group.columns.length, 0);
-
-            const sheetId = await sheet.addSheet({ title: page.title, columns: columnCount, rows: page.rows, color: page.tabColor });
-
-            if (sheetId == undefined)
-                throw new Error("Failed to add sheet");
-
-            await sheet.modify(r => {
-                const multigroup = page.groups.length > 1;
-
-                let index = 0;
-
-                for (const group of page.groups) {
-                    if (multigroup) {
-                        r = r
-                            .mergeCells(sheetId, SheetRange.row(0, index, group.columns.length))
-                            .updateCells(sheetId, SheetRange.cell(index, 0), { value: group.title, horizontal: 'middle', vertical: 'middle', ...group.titleStyle });
-
-                        if (group.titleStyle?.beneath)
-                            r = r.setBorder(sheetId, SheetRange.row(0, index, group.columns.length), { bottom: group.titleStyle.beneath });
-
-                        if (group.titleStyle?.between)
-                            r = r.setBorder(sheetId, SheetRange.region(index, 0, group.columns.length, page.rows), { left: group.titleStyle.between, right: group.titleStyle.between });
-                    }
-
-                    for (let c = 0; c < group.columns.length; c++) {
-                        const rowOffset = multigroup ? 1 : 0;
-
-                        const column = group.columns[c];
+            await sheet.modify(r => r.setTitle(book.title));
 
 
-                        // header
-                        r = r.updateCells(sheetId, SheetRange.cell(index, rowOffset), {
-                            value: column.title,
-                            horizontal: 'middle', vertical: 'middle',
-                            ...column.titleStyle
-                        });
+            for (const page of book.pages) {
+                const columnCount = page.groups.reduce((acc, group) => acc + group.columns.length, 0);
 
+                const sheetId = await sheet.addSheet({ title: page.title, columns: columnCount, rows: page.rows, color: page.tabColor });
 
-                        // data
-                        const columnRange = SheetRange.column(index, rowOffset + 1, page.rows - 1 - rowOffset);
+                if (sheetId == undefined)
+                    throw new Error("Failed to add sheet");
 
-                        r = r.updateCells(sheetId, columnRange, {
-                            horizontal: 'middle', vertical: 'middle',
-                            ...column.dataStyle,
-                            value: column.formula,
-                            kind: column.behavior?.kind,
-                            format: column.behavior?.format
-                        });
+                await sheet.modify(r => {
+                    const multigroup = page.groups.length > 1;
 
-                        if (column.behavior?.rule)
-                            r = r.setDataValidation(sheetId, columnRange, column.behavior.rule, true);
+                    let index = 0;
 
-                        if (column.behavior?.styles)
-                            for (const format of column.behavior.styles)
-                                r = r.setConditionalFormat(sheetId, columnRange, format);
+                    for (const group of page.groups) {
+                        if (multigroup) {
+                            r = r
+                                .mergeCells(sheetId, SheetRange.row(0, index, group.columns.length))
+                                .updateCells(sheetId, SheetRange.cell(index, 0), { value: group.title, horizontal: 'middle', vertical: 'middle', ...group.titleStyle });
 
-                        if (column.titleStyle?.beneath)
-                            r = r.setBorder(sheetId, SheetRange.cell(index, rowOffset), { bottom: column.titleStyle.beneath });
+                            if (group.titleStyle?.beneath)
+                                r = r.setBorder(sheetId, SheetRange.row(0, index, group.columns.length), { bottom: group.titleStyle.beneath });
 
-
-                        if (column.titleStyle?.between) {
-                            const showLeft = c !== 0;
-                            const showRight = c != group.columns.length - 1;
-
-                            if (showLeft || showRight)
-                                r = r.setBorder(sheetId, SheetRange.region(index, rowOffset, 1, page.rows - rowOffset), {
-                                    left: showLeft ? column.titleStyle.between : undefined,
-                                    right: showRight ? column.titleStyle.between : undefined
-                                });
+                            if (group.titleStyle?.between)
+                                r = r.setBorder(sheetId, SheetRange.region(index, 0, group.columns.length, page.rows), { left: group.titleStyle.between, right: group.titleStyle.between });
                         }
 
-                        index++;
+                        for (let c = 0; c < group.columns.length; c++) {
+                            const rowOffset = multigroup ? 1 : 0;
+
+                            const column = group.columns[c];
+
+
+                            // header
+                            r = r.updateCells(sheetId, SheetRange.cell(index, rowOffset), {
+                                value: column.title,
+                                horizontal: 'middle', vertical: 'middle',
+                                ...column.titleStyle
+                            });
+
+
+                            // data
+                            const columnRange = SheetRange.column(index, rowOffset + 1, page.rows - 1 - rowOffset);
+
+                            r = r.updateCells(sheetId, columnRange, {
+                                horizontal: 'middle', vertical: 'middle',
+                                ...column.dataStyle,
+                                value: column.formula,
+                                kind: column.behavior?.kind,
+                                format: column.behavior?.format
+                            });
+
+                            if (column.behavior?.rule)
+                                r = r.setDataValidation(sheetId, columnRange, column.behavior.rule, true);
+
+                            if (column.behavior?.styles)
+                                for (const format of column.behavior.styles)
+                                    r = r.setConditionalFormat(sheetId, columnRange, format);
+
+                            if (column.titleStyle?.beneath)
+                                r = r.setBorder(sheetId, SheetRange.cell(index, rowOffset), { bottom: column.titleStyle.beneath });
+
+
+                            if (column.titleStyle?.between) {
+                                const showLeft = c !== 0;
+                                const showRight = c != group.columns.length - 1;
+
+                                if (showLeft || showRight)
+                                    r = r.setBorder(sheetId, SheetRange.region(index, rowOffset, 1, page.rows - rowOffset), {
+                                        left: showLeft ? column.titleStyle.between : undefined,
+                                        right: showRight ? column.titleStyle.between : undefined
+                                    });
+                            }
+
+                            index++;
+                        }
                     }
-                }
 
-                return r;
-            });
+                    return r;
+                });
+            }
+
+            // remove the reset sheet
+            await sheet.modify(r => r.dropSheets(resetSheetId));
+
+            return Result.success(undefined);
         }
-
-        // remove the reset sheet
-        await sheet.modify(r => r.dropSheets(resetSheetId));
-
-        return Result.success(undefined);
+        catch (error: any) {
+            return Result.failure([{ type: 'generating', message: error.message, data: book }]);
+        }
     }
 }
