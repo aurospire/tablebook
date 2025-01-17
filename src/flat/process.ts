4868f12a39efc87813abb1449ad882591d5630ea
aurospire <aurospire@gmail.com>
2025-01-17 03:30:11 -0500

    Finished processFlatBook

--- src/flat/process.ts
+++ src/flat/process.ts
@@ -1,13 +1,14 @@
 import { TableBookProcessIssue } from "../issues";
-import { Reference, TableBook, TableGroup, TablePage, EnumType, TableColumn, FlatExpression } from "../tables";
+import { Reference, TableBook, TableGroup, TablePage, EnumType, TableColumn, FlatExpression, ColumnType } from "../tables";
 import { Result } from "../util";
-import { FlatBook } from "./types";
+import { FlatBook, FlatEnumTypeRegex, FlatFormulaSourceRegex, FlatNumericTypeRegex, FlatTypes } from "./types";
 
 export const processFlatBook = (book: FlatBook): Result<TableBook, TableBookProcessIssue[]> => {
     const issues: TableBookProcessIssue[] = [];
 
     const pages: TablePage[] = [];
     const enumMap: Record<string, EnumType> = {};
+    const types: Record<string, ColumnType> = {};
     const formulaMap: Record<string, FlatExpression> = {};
     const pageMap: Record<string, TablePage> = {};
     const groupMap: Record<string, TableGroup> = {};
@@ -32,7 +33,8 @@ export const processFlatBook = (book: FlatBook): Result<TableBook, TableBookProc
     }
 
     for (const flatEnum of book.enums) {
-        const tableEnum: EnumType = (enumMap[flatEnum.name] ?? (enumMap[flatEnum.name] = { kind: 'enum', items: [] }));
+        const name = flatEnum.name + 'enum';
+        const tableEnum: EnumType = (enumMap[name] ?? (enumMap[name] = { kind: 'enum', items: [] }));
 
         tableEnum.items.push({
             name: flatEnum.value,
@@ -102,11 +104,116 @@ export const processFlatBook = (book: FlatBook): Result<TableBook, TableBookProc
             else if (columnSet.has(columnKey))
                 issues.push({ type: 'processing', message: 'Duplicate Column', data: flatColumn, path: ['columns', c] });
             else {
+
+                let type: Reference;
+
+                // TODO: Have defaults, but allow custom overrides
+                switch (flatColumn.type) {
+                    case 'text':
+                        type = '@text';
+                        if (!('text' in types))
+                            types['text'] = { kind: 'text' };
+                        break;
+                    case 'number':
+                        type = '@number';
+                        if (!('number' in types))
+                            types['number'] = { kind: 'numeric', format: { type: 'number', commas: true } };
+                        break;
+                    case 'currency':
+                        type = '@currency';
+                        if (!('currency' in types))
+                            types['currency'] = { kind: 'numeric', format: { type: 'currency', commas: true, decimal: 2, symbol: '$' } };
+                        break;
+                    case 'percent':
+                        type = '@percent';
+                        if (!('percent' in types))
+                            types['percent'] = { kind: 'numeric', format: { type: 'percent', commas: true } };
+                        break;
+                    case 'date':
+                        type = '@date';
+                        if (!('date' in types))
+                            // Sun, Jan 01, 1900
+                            types['date'] = {
+                                kind: 'temporal',
+                                format: [
+                                    { type: 'weekday', length: 'short' },
+                                    ', ',
+                                    { type: 'month', length: 'short' },
+                                    ' ',
+                                    { type: 'day', length: 'long' },
+                                    ', ',
+                                    { type: 'year', length: 'long' }
+                                ]
+                            };
+                        break;
+                    case 'datetime':
+                        type = '@datetime';
+                        // 2021-01-01T00:00:00
+                        if (!('datetime' in types))
+                            types['datetime'] = {
+                                kind: 'temporal',
+                                format: [
+                                    { type: 'year', length: 'long' },
+                                    '-',
+                                    { type: 'month', length: 'long' },
+                                    '-',
+                                    { type: 'day', length: 'long' },
+                                    'T',
+                                    { type: 'hour', length: 'long' },
+                                    ':',
+                                    { type: 'minute', length: 'long' },
+                                    ':',
+                                    { type: 'second', length: 'long' }
+                                ]
+                            };
+                        break;
+                    default: {
+                        let match;
+
+                        if (match = flatColumn.type.match(FlatNumericTypeRegex)) {
+                            const typename = match[1];
+                            const decimals = Number(match[2]);
+
+                            const fulltypename = typename + decimals;
+
+                            type = '@' + fulltypename;
+
+                            if (!(fulltypename in types))
+                                types[fulltypename] = { kind: 'numeric', format: { type: typename as any, commas: true, decimal: decimals } };
+                        }
+                        else if (match = flatColumn.type.match(FlatEnumTypeRegex)) {
+                            const typename = match[1];
+
+                            type = '@' + typename + 'enum';
+
+                            if (!(typename in enumMap))
+                                issues.push({ type: 'processing', message: 'Invalid Enum Reference', data: flatColumn.type, path: ['columns', c] });
+                        }
+                        else {
+                            issues.push({ type: 'processing', message: 'Invalid Column Type', data: flatColumn.type, path: ['columns', c] });
+                            type = '@text';
+                        }
+                    }
+                }
+
+                let source: string | undefined;
+                let expression: FlatExpression | undefined;
+
+                let match = flatColumn.source.match(FlatFormulaSourceRegex);
+
+                if (match) {
+                    source = 'auto';
+                    expression = formulaMap[match[1]];
+                    if (!expression)
+                        issues.push({ type: 'processing', message: 'Invalid Formula Reference', data: flatColumn.source, path: ['columns', c] });
+                }
+
                 const column: TableColumn = {
                     name: flatColumn.name,
                     description: flatColumn.description,
-                    source: flatColumn.source,
-
+                    type: type as Reference,
+                    expression,
+                    source
 
                 };
 
